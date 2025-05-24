6ab313b294e1bfef694856db0947108a82491734
winTessy <tesstesstex@gmail.com>
2025-05-24 20:34:54 +0900

    Added settings to deploy by Github Pages

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -2,8 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: '/dashboard/',
-  basePath: '/dashboard',
+  // GitHub Pages URL will be https://<username>.github.io/finance-dashboard/
+  // So, basePath and assetPrefix should be /finance-dashboard
+  assetPrefix: '/finance-dashboard/',
+  basePath: '/finance-dashboard',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,    // Required for next export with next/image
+  }
   /* config options here */
 };
 
